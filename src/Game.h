ddf12e12faf168264de428ce7cb90f758d337d59
vyasmanav <manav2306@gmail.com>
2025-10-25 13:11:20 +0530

    Added new feature: pause control

--- src/Game.h
+++ src/Game.h
@@ -15,6 +15,7 @@ private:
     HighScore highScore;
     int score;
     bool gameOver;
+    bool paused;  // Add paused state
     
     void Draw();
     void Input();
@@ -22,6 +23,7 @@ private:
     void GameOverScreen();
     void EnterHighScore();
     void Reset();
+    void PauseGame();  // Add pause method
     
 public:
     Game();
